32b4af21364b40f829dd295a303e1403f48c4d1e
Moritz Zeitler <moritz.zeitler@web.de>
2021-06-02 15:43:25 +0200

    adding stuff

---
+++ Python/MoeWorkspace/test.py
@@ -0,0 +1,17 @@
+import numpy as np
+import glob
+import cv2
+ 
+img_array = []
+for filename in glob.glob('C:/Users/Moe/Desktop/disparitySample2/*.png'):
+    img = cv2.imread(filename)
+    height, width, layers = img.shape
+    size = (width,height)
+    img_array.append(img)
+ 
+ 
+out = cv2.VideoWriter('project.avi',cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
+ 
+for i in range(len(img_array)):
+    out.write(img_array[i])
+out.release()
---
+++ Python/oldStuff/DisparityDepthTests/DepthTest2.py
@@ -0,0 +1,149 @@
+import numpy as np
+import cv2
+import json
+
+
+def load_stereo_coefficients(path):
+    """ Loads stereo matrix coefficients. """
+    # FILE_STORAGE_READ
+    cv_file = cv2.FileStorage(path, cv2.FILE_STORAGE_READ)
+
+    # note we also have to specify the type to retrieve other wise we only get a
+    # FileNode object back instead of a matrix
+    K1 = cv_file.getNode("K1").mat()
+    D1 = cv_file.getNode("D1").mat()
+    K2 = cv_file.getNode("K2").mat()
+    D2 = cv_file.getNode("D2").mat()
+    R = cv_file.getNode("R").mat()
+    T = cv_file.getNode("T").mat()
+    E = cv_file.getNode("E").mat()
+    F = cv_file.getNode("F").mat()
+    R1 = cv_file.getNode("R1").mat()
+    R2 = cv_file.getNode("R2").mat()
+    P1 = cv_file.getNode("P1").mat()
+    P2 = cv_file.getNode("P2").mat()
+    Q = cv_file.getNode("Q").mat()
+
+    cv_file.release()
+    return [K1, D1, K2, D2, R, T, E, F, R1, R2, P1, P2, Q]
+
+
+CamL = cv2.VideoCapture(
+    'C:\\Users\\Moe\\Desktop\\UniversityCoursesGit\\SpeedNeverKilledSomebody\\CapturedVideos\\sampleLeft.avi')
+CamR = cv2.VideoCapture(
+    'C:\\Users\\Moe\\Desktop\\UniversityCoursesGit\\SpeedNeverKilledSomebody\\CapturedVideos\\sampleRight.avi')
+
+
+def save_config(v):
+    config_map = {}
+    config_map['numDisparities'] = numDisparities / 16
+    config_map['blocksize'] = (blockSize - 5) / 2
+    config_map['preFilterType'] = preFilterType
+    config_map['preFilterSize'] = (preFilterSize - 5) / 2
+    config_map['preFilterCap'] = preFilterCap
+    config_map['textureThreshold'] = textureThreshold
+    config_map['uniquenessRatio'] = uniquenessRatio
+    config_map['speckleRange'] = speckleRange
+    config_map['speckleWindowSize'] = speckleWindowSize / 2
+    config_map['disp12MaxDiff'] = disp12MaxDiff
+    config_map['minDisparity'] = minDisparity
+    with open('config.json', 'w') as fp:
+        json.dump(config_map, fp)
+
+
+def nothing(x):
+    pass
+
+
+cv2.namedWindow('disp', cv2.WINDOW_NORMAL)
+cv2.resizeWindow('disp', 400, 400)
+
+cv2.namedWindow('disp2', cv2.WINDOW_NORMAL)
+cv2.resizeWindow('disp2', 600, 600)
+
+cv2.createTrackbar('numDisparities', 'disp', 1, 17, nothing)
+cv2.createTrackbar('blockSize', 'disp', 5, 50, nothing)
+cv2.createTrackbar('preFilterType', 'disp', 1, 1, nothing)
+cv2.createTrackbar('preFilterSize', 'disp', 2, 25, nothing)
+cv2.createTrackbar('preFilterCap', 'disp', 5, 62, nothing)
+cv2.createTrackbar('textureThreshold', 'disp', 10, 100, nothing)
+cv2.createTrackbar('uniquenessRatio', 'disp', 15, 100, nothing)
+cv2.createTrackbar('speckleRange', 'disp', 0, 100, nothing)
+cv2.createTrackbar('speckleWindowSize', 'disp', 3, 25, nothing)
+cv2.createTrackbar('disp12MaxDiff', 'disp', 5, 25, nothing)
+cv2.createTrackbar('minDisparity', 'disp', 5, 25, nothing)
+cv2.createTrackbar('saveConfig', 'disp', 1, 1, lambda v: save_config(v))
+
+# Creating an object of StereoBM algorithm
+stereo = cv2.StereoBM_create()
+
+K1, D1, K2, D2, R, T, E, F, R1, R2, P1, P2, Q = load_stereo_coefficients('stereo.yml')  # Get cams params
+
+while True:
+
+    # Capturing and storing left and right camera images
+    retL, imgL = CamL.read()
+    retR, imgR = CamR.read()
+
+    # Proceed only if the frames have been captured
+    if retL and retR:
+        imgR_gray = cv2.cvtColor(imgR, cv2.COLOR_BGR2GRAY)
+        imgL_gray = cv2.cvtColor(imgL, cv2.COLOR_BGR2GRAY)
+
+        #leftMapX, leftMapY = cv2.initUndistortRectifyMap(K1, D1, R1, P1, (640, 400), cv2.CV_32FC1)
+        #left_rectified = cv2.remap(imgL_gray, leftMapX, leftMapY, cv2.INTER_LINEAR, cv2.BORDER_CONSTANT)
+        #rightMapX, rightMapY = cv2.initUndistortRectifyMap(K2, D2, R2, P2, (640, 400), cv2.CV_32FC1)
+        #right_rectified = cv2.remap(imgR_gray, rightMapX, rightMapY, cv2.INTER_LINEAR, cv2.BORDER_CONSTANT)
+
+        # Updating the parameters based on the trackbar positions
+        numDisparities = cv2.getTrackbarPos('numDisparities', 'disp') * 16
+        blockSize = cv2.getTrackbarPos('blockSize', 'disp') * 2 + 5
+        preFilterType = cv2.getTrackbarPos('preFilterType', 'disp')
+        preFilterSize = cv2.getTrackbarPos('preFilterSize', 'disp') * 2 + 5
+        preFilterCap = cv2.getTrackbarPos('preFilterCap', 'disp')
+        textureThreshold = cv2.getTrackbarPos('textureThreshold', 'disp')
+        uniquenessRatio = cv2.getTrackbarPos('uniquenessRatio', 'disp')
+        speckleRange = cv2.getTrackbarPos('speckleRange', 'disp')
+        speckleWindowSize = cv2.getTrackbarPos('speckleWindowSize', 'disp') * 2
+        disp12MaxDiff = cv2.getTrackbarPos('disp12MaxDiff', 'disp')
+        minDisparity = cv2.getTrackbarPos('minDisparity', 'disp')
+
+        # Setting the updated parameters before computing disparity map
+        stereo.setNumDisparities(numDisparities)
+        stereo.setBlockSize(blockSize)
+        stereo.setPreFilterType(preFilterType)
+        stereo.setPreFilterSize(preFilterSize)
+        stereo.setPreFilterCap(preFilterCap)
+        stereo.setTextureThreshold(textureThreshold)
+        stereo.setUniquenessRatio(uniquenessRatio)
+        stereo.setSpeckleRange(speckleRange)
+        stereo.setSpeckleWindowSize(speckleWindowSize)
+        stereo.setDisp12MaxDiff(disp12MaxDiff)
+        stereo.setMinDisparity(minDisparity)
+
+        # Calculating disparity using the StereoBM algorithm
+        disparity = stereo.compute(imgL_gray, imgR_gray)
+        disparity = np.uint8(disparity)
+        # NOTE: Code returns a 16bit signed single channel image,
+        # CV_16S containing a disparity map scaled by 16. Hence it
+        # is essential to convert it to CV_32F and scale it down 16 times.
+        #disparity = cv2.applyColorMap(disparity, cv2.COLORMAP_DEEPGREEN)
+        # Converting to float32
+        disparity = disparity.astype(np.float32)
+
+        # Scaling down the disparity values and normalizing them
+        disparity = (disparity / 16.0 - minDisparity) / numDisparities
+
+        # Displaying the disparity map
+        cv2.imshow("disp", disparity)
+        cv2.imshow("disp2", imgL_gray)
+
+        # Close window using esc key
+        if cv2.waitKey(50) == 27:
+            break
+    else:
+        print('no video')
+        CamL.set(cv2.CAP_PROP_POS_FRAMES, 0)
+        CamR.set(cv2.CAP_PROP_POS_FRAMES, 0)
+
+print()
